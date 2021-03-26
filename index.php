e23e544b08d746b2d9f22abe810d65f34f31c804
alepoo84 <81421236+alepoo84@users.noreply.github.com>
2021-03-26 15:17:57 +0300

    بوت الباحث القرآني على التلجرام2021

---
+++ index.php
@@ -0,0 +1,40 @@
+'<div dir="rtl" style="text-align: right;" trbidi="on">
+<div class="separator" style="clear: both; text-align: center;">
+</div>
+<div style="margin-left: 1em; margin-right: 1em;">
+</div>
+<div style="text-align: center;">
+<span style="color: purple; font-size: x-large;">أهلا وسهلا بك في الموقع الرسمي لـ </span><b style="font-size: xx-large;"><span style="color: #351c75; font-family: &quot;verdana&quot; , sans-serif;"><a href="https://t.me/SJAD100">،ﻔـړﺂﻏﻎ! 𖤍3.O.</a></span></b><span style="color: purple; font-size: x-large;">&nbsp;😍💜</span></div>
+<span style="color: purple; font-size: x-large;"><br /></span>
+<br />
+<div class="separator" style="clear: both; text-align: center;">
+<a href="https://upupp.000webhostapp.com/Vy6/IMG_20181216_234327_622.jpg" imageanchor="1" style="margin-left: 1em; margin-right: 1em;"><img alt="ÙØªÙØ¬Ø© Ø¨Ø­Ø« Ø§ÙØµÙØ± Ø¹Ù âªComputer icon.pngâ¬â" border="0" height="320" src="https://cc-ll.000webhostapp.com/Vy6/IMG_20181216_234327_622.jpg" width="320" /></a></div>
+<div class="separator" style="clear: both; text-align: center;">
+<br /></div>
+<div class="separator" style="clear: both; text-align: center;">
+<br /></div>
+<div style="text-align: center;">
+<div style="text-align: right;">
+<div style="text-align: center;">
+    <a href="https://t.me/iBadlz" style="font-size: xx-large;"</a></div
+>
+<div style="text-align: center;">
+<div style="text-align: right;">
+<div style="text-align: center;">
+<a href="https://t.me/iBadlz" style="font-size: xx-large;">اضغط هنا وتابع جديدنا 💜</a></div>
+</div>
+</div>
+<br />
+<span style="background-color: white; font-family: &quot;roboto&quot; , sans-serif; font-size: 26px; font-weight: 700; text-align: center;"><span style="color: #741b47;"><br /></span></span>
+<br />
+<div style="text-align: right;">
+<div style="text-align: right;">
+<div style="text-align: right;">
+<span style="background-color: white; font-family: &quot;roboto&quot; , sans-serif; font-size: 26px; font-weight: 700; text-align: center;"><span style="color: #741b47;"><span style="background-color: white; font-family: &quot;roboto&quot; , sans-serif; font-size: 26px; font-weight: 700; text-align: center;"><span style="color: #741b47;">
+    <div style="text-align: center;">
+<div style="text-align: right;">
+<div style="text-align: center;">
+    <a href="https://t.me/JJ117">غغـراضيـﮧٓي ،†</a></span></span><span style="background-color: white; color: #333333; font-family: &quot;roboto&quot; , sans-serif; font-size: 26px; font-weight: 700; text-align: center;"><a href="https://t.me/iBadlz"> 🍃✨</a></span></span></span></div>
+</div>
+</div>
+</div>
